d4454e3b0aff6e5ce9885c9e5d78d4921bc9154c
continuousone <65003681+continuousone@users.noreply.github.com>
2024-12-30 10:19:23 -0800

    Added javascript to home scoreboard

---
+++ scoreboard/index.js
@@ -0,0 +1,21 @@
+
+let homescoreEl = document.getElementById("homescore-el")
+
+let score = 0
+
+function add1(){
+    score +=1
+    homescoreEl.textContent = score
+
+}
+
+function add2(){
+    score +=2
+    homescoreEl.textContent = score
+
+}
+function add3(){
+    score +=3
+    homescoreEl.textContent = score
+
+}
